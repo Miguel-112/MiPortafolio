7a6d681f3ab5eb6db45e6fda1c8551e6dd3a293c
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2023-04-30 15:41:11 -0500

    Apartado about me  culminado.
    Apartado about me  culminado.

--- MiPortafolio/src/app/component/sidenav/sidenav.component.ts
+++ MiPortafolio/src/app/component/sidenav/sidenav.component.ts
@@ -55,6 +55,9 @@ export class SidenavComponent {
 
   collapsed = false;
   screenWidth = 0;
+  descripciontitulo='Soy un autodidacta con gran capacidad de aprendizaje y una pasión por la programación. Me encanta enfrentarme a problemas complejos y desafiantes del mundo real, ya que mi mente curiosa siempre está buscando soluciones innovadoras. Además, tengo un gran compromiso con la mejora continua, lo que me impulsa a seguir aprendiendo y perfeccionando mis habilidades como programador.';
+  public lineas = ["Ingeniero", "Entusiasta", "Desarrollador web"];
+
 
   navData = navbarData;
 
@@ -63,8 +66,47 @@ export class SidenavComponent {
     this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth:this.screenWidth});
   }
 
+  ngOnInit() {
+  
+ 
+    this.mostrarSiguienteLinea();
+    }
+
   closeSidenav() {
     this.collapsed = false;
     this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth:this.screenWidth});
   }
+
+
+
+
+  public mostrarSiguienteLinea = () => {
+    const contenedor = document.getElementById("contenedor") as HTMLSpanElement;
+    let i = 0;
+    const mostrarLinea = () => {
+      const lineaActual = this.lineas[i];
+      for (let j = lineaActual.length; j >= 0; j--) {
+        setTimeout(() => {
+          contenedor.textContent = lineaActual.slice(0, j);
+        }, (lineaActual.length - j) * 50);
+      }
+      i++;
+      if (i >= this.lineas.length) {
+        i = 0;
+      }
+      setTimeout(() => {
+        const siguienteLinea = this.lineas[i];
+        contenedor.textContent = siguienteLinea;
+        contenedor.style.borderRight = "1px solid transparent";
+        setTimeout(() => {
+          contenedor.style.borderRight = "1px solid white";
+        }, 500);
+      }, lineaActual.length * 50 + 150);
+      setTimeout(mostrarLinea, (lineaActual.length + 1) * 50 + 2850);
+    };
+    mostrarLinea();
+  }
+  
+  
+  
 }
